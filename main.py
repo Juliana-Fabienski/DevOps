adaf9cf51c1bd1053b0b8fb051b5f76bfe46b65e
Juliana Fabienski <juliana.camargo2912@gmail.com>
2026-04-25 11:43:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Estudante(BaseModel):
+    nome: str
+    curso: str
+    ativo: bool
 
 @app.get("/")
 async def root():
@@ -12,3 +16,15 @@ async def root():
 @app.get("/teste")
 async def funcao():
     return {True, "num_random":random.randint(0, 500)}
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_estudante(id_estudante: int):
+    return id_estudante > 0
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
